70646bf5f64091246b096765b1d63331f5a4b4aa
Anton Rassadin <toha90@gmail.com>
2018-11-30 12:22:30 +0300

    Submodule update & Android build script

---
+++ PsDemo/Build/bamboo/build-android.sh
@@ -0,0 +1,107 @@
+set -e
+
+grepCheck()
+{
+    if [ "${4}" = true ] ; then
+        if grep -q ${1} ${2}
+        then
+            echo "Error grep: build failed in line ${3}, found ${1} at ${2}"
+            exit 1
+        fi
+    else
+        if ! grep -q ${1} ${2}; then
+            echo "Error grep: build failed in line ${3}, couldn't found ${1} at ${2}"
+            exit 1
+        fi
+    fi
+}
+
+cd PsDemo
+
+##
+## CLEANUP
+##
+
+echo "Remove old SVN content.."
+
+# delete common data from svn
+rm -rf Binaries
+
+# cleanup if necessary
+if [ "${bamboo_BUILD_CLEAN}" -eq "1" ]
+then
+    echo "Force clean build.."
+    rm -rf DerivedDataCache Intermediate Plugins Saved
+fi
+
+echo "Check dirs after cleanup:"
+ls -al
+
+##
+## BUILD CONFIGURATION
+##
+
+echo "Update Build Configuration.."
+
+# echo "Set build configuration.."
+grepCheck "BuildConfiguration=PPBC_Development" Config/DefaultGame.ini ${LINENO}
+sed -i -e "s|BuildConfiguration=PPBC_Development|BuildConfiguration=PPBC_${bamboo_COOK_CLIENTCONFIG}|" Config/DefaultGame.ini
+grep BuildConfiguration Config/DefaultGame.ini
+
+# check full rebuild
+if [ "${bamboo_BUILD_CLEAN}" -eq "1" ]
+then
+    echo "Force full rebuild.."
+    grepCheck "FullRebuild=False" Config/DefaultGame.ini ${LINENO}
+    sed -i -e "s|FullRebuild=False|FullRebuild=True|" Config/DefaultGame.ini
+    grep FullRebuild Config/DefaultGame.ini
+fi
+
+export JAVA_OPTS="-Xms10g -Xmx12g $JAVA_OPTS"
+JAVA_OPTS="-Xms10g -Xmx12g $JAVA_OPTS"
+SUPPORTED_TEXTURES=ETC2
+
+##
+## BUILD COOK
+##
+
+echo "Build Cook Run.."
+
+UE4_ROOT="${bamboo_capability_system_ue4new}"
+UE4_PROJECT_ROOT=$(pwd)
+UE4_PROJECT_FILE="${UE4_PROJECT_ROOT}/PsDemo.uproject"
+
+echo "UE4 Project file ${UE4_PROJECT_FILE}"
+echo "UE4 ROOT: $UE4_ROOT"
+
+echo "Unlock the keychain to make code signing work.."
+security unlock-keychain -p awmcodemac ${HOME}/Library/Keychains/login.keychain
+
+echo "Build now.."
+pushd ${UE4_ROOT}
+./Engine/Build/BatchFiles/RunUAT.command BuildCookRun \
+   "-project=${UE4_PROJECT_FILE}" -noP4 \
+   -clientconfig=${bamboo_COOK_CLIENTCONFIG} -seconfig=${bamboo_COOK_SERVERCONFIG} -utf8output -platform=Android \
+   -targetplatform=Android -cookflavor=${SUPPORTED_TEXTURES} \
+   -build -cook -ForceUnity \
+   -unversionedcookedcontent -compressed -stage -package \
+   -prereqs -archive -archivedirectory=${UE4_PROJECT_ROOT}/build \
+
+if [ ! $? -eq 0 ]; then
+    echo "Error: build failed"
+    exit 1
+fi
+
+popd
+
+##
+## END
+##
+
+echo "Clean build logs in $HOME/Library/Logs/Unreal Engine/LocalBuildLogs/"
+if [ -e "$HOME/Library/Logs/Unreal Engine/LocalBuildLogs/" ] ; then
+    PREV=`pwd`
+    cd "$HOME/Library/Logs/Unreal Engine/LocalBuildLogs/"
+    find . -type f -mtime +7 -exec rm {} \;
+    cd "$PREV"
+fi
